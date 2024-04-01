4d62145b46f5b36876ab756b5df5fe4dd2b98b0a
moonsung1 <anstjddnjs1006@naver.com>
2024-04-01 12:40:28 +0900

    홈페이지를 localhost:1004로 옮김

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();         
+const express = require('express');   //express 모듈 부름
+const app = express();   // app이 express 객체      
 
 app.listen(1004, function(){
     console.log('listening on 1004')      //포트 접속하면 콘솔창에 메시지 출력
@@ -8,12 +8,11 @@ app.listen(1004, function(){
 });                 // 여기까지 express 기본문법    1004 port 서버 띄워주세요
 
 
-//누군가가 /pet 으로 방문을 하면`
+//누군가가 /pet 으로 방문을 하면
 // pet 관련된 안내문을 띄워주자
 
 app.get('/pet', function(req, res){
     res.send('펫용품 페이지입니다.');
-
 });
 
 app.get('/', function(req, res){         //     /는 홈페이지.    
