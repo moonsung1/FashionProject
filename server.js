e91437ee84df4667f6158e96a7f72a5f035621ea
moonsung1 <anstjddnjs1006@naver.com>
2024-04-01 14:50:25 +0900

    이미지 처리 오류 해결

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');   //express 모듈 부름
-const app = express();   // app이 express 객체      
+var http = require('http')             //기본적으로 깔리는 모듈
+var express = require('express');   //express 모듈 부름
+var app = express();   // app이 express 객체      
+var fs = require('fs');
 
 app.listen(1004, function(){
     console.log('listening on 1004')      //포트 접속하면 콘솔창에 메시지 출력
@@ -16,7 +18,34 @@ app.get('/pet', function(req, res){
 });
 
 app.get('/', function(req, res){         //     /는 홈페이지.    
-    res.sendFile(__dirname + '/mainpage.html');      //html을 전송
+    res.sendFile(__dirname + '/mainpage.html'); 
+     });    //html을 전송
+
+app.get('/images', function(req,res){
+
+    fs.readFile('./images/denim.jpg', function(err,data){
+     res.writeHead(200, {'Content-Type': 'images/jpg'});
+     res.end(data);
+});
+
+});
+
+app.get('/image', function(req,res){
+
+    fs.readFile('./images/북한돼지.jpg', function(err,data){
+     res.writeHead(200, {'Content-Type': 'images/jpg'});
+     res.end(data);
+});
+
 });
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
