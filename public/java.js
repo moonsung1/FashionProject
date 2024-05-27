509d6bcb50aa1d1338bb694a5ed6f35da454fcdb
moonsung1 <anstjddnjs1006@naver.com>
2024-05-27 22:45:28 +0900

    css, javascript 파일 public폴더 안에 넣어 연동

---
+++ public/java.js
@@ -0,0 +1,15 @@
+document.getElementById('homeBtn').addEventListener('click', function() {
+window.location.href = '/';
+});
+
+document.getElementById('closetBtn').addEventListener('click', function() {
+    window.location.href = '/closet';
+});
+
+document.getElementById('pickolorBtn').addEventListener('click', function() {
+    window.location.href = '/pickolor';
+});
+
+document.getElementById('signupBtn').addEventListener('click', function() {
+    window.location.href = '/signup';
+});
